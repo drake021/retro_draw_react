55a0bfe659521320c3380f0a7167b38ff09f9a69
drake021 <68008325+drake021@users.noreply.github.com>
2021-01-31 18:20:04 -0500

    project upload

---
+++ precache-manifest.4fe7c1b487a069b0180e0c9971b66d10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e21db8e165c5d4c7fa8264ee76c84be8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "289d2c07d513d629f353",
+    "url": "/static/js/2.4bf62a69.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.4bf62a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d470fa10cd3ecbf400ed",
+    "url": "/static/js/main.28e93336.chunk.js"
+  },
+  {
+    "revision": "1316b828eda579fe06fd",
+    "url": "/static/js/runtime-main.86007643.js"
+  }
+]);
